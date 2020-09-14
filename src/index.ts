1c4de17a2ead4ac1397c428736d3f94a5729b204
Archit Bhonsle <abhonsle2000@gmail.com>
2020-09-14 11:47:24 +0530

    fixing merge mistakes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import mongoose from "mongoose";
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { buildSchema } from "type-graphql";
-import "reflect-metadata";
-import { HelloResolver } from "./resolvers/hello";
-import { UserResolver } from "./resolvers/user";
-=======
 import 'reflect-metadata';
 import mongoose from 'mongoose';
 import express from 'express';
@@ -16,30 +7,16 @@ import { UserResolver } from './resolvers/user';
 import { ItemResolver } from './resolvers/item';
 import { getTokenFromHeader, getUsernameFromToken } from './utils/jwt';
 import { seed } from './utils/seed';
-<<<<<<< HEAD
 import { MyContext } from './utils/misc';
-=======
->>>>>>> da8f112dd8f9a1e11f95947895a0d2ac8345711e
->>>>>>> 98b7fb90ab3175a9f24d8c29c7d49c4a1d73151c
 
 const main = async () => {
-  await mongoose.connect("mongodb://localhost/inventory-management", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  });
+    await mongoose.connect('mongodb://localhost/inventory-management', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    });
 
-<<<<<<< HEAD
-  const app = express();
-
-  const apolloServer = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [HelloResolver, UserResolver],
-      validate: false,
-    }),
-  });
-=======
     await seed();
 
     const app = express();
@@ -54,13 +31,12 @@ const main = async () => {
             return { username };
         }
     });
->>>>>>> da8f112dd8f9a1e11f95947895a0d2ac8345711e
 
-  apolloServer.applyMiddleware({ app });
+    apolloServer.applyMiddleware({ app });
 
-  app.listen(4000, () => {
-    console.log("🚀 at http://localhost:4000/graphql");
-  });
+    app.listen(4000, () => {
+        console.log('🚀 at http://localhost:4000/graphql');
+    });
 };
 
 main();
