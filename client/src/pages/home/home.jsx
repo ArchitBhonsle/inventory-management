cd5952bbc466e741ae9a4d2a98fc91436cbc413c
Dion Pinto <dionpinto000@gmail.com>
2020-09-25 18:46:53 +0530

    working profile compoenent

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //material ui
 import { Grid, Paper } from "@material-ui/core";
@@ -7,8 +7,27 @@ import { Grid, Paper } from "@material-ui/core";
 import Profile from "../../components/Profile/Profile";
 import List from "../../components/List/List";
 
-const home = () => {
-  return (
+//graphql
+import { gql, useLazyQuery, useQuery } from "@apollo/client";
+
+const ME_QUERY = gql`
+  query {
+    me {
+      username
+      items
+      bio
+      department
+      image
+    }
+  }
+`;
+
+const Home = () => {
+  const { data } = useQuery(ME_QUERY);
+
+  console.log(data);
+
+  const render = data ? (
     <Grid container spacing={4}>
       <Grid item sm={8} xs={12}>
         <h1>Current Items</h1>
@@ -21,10 +40,14 @@ const home = () => {
         </Paper>
       </Grid>
       <Grid item sm={4} xs={12}>
-        <Profile />
+        <Profile data={data.me} />
       </Grid>
     </Grid>
+  ) : (
+    <h1>Loading</h1>
   );
+
+  return render;
 };
 
-export default home;
+export default Home;
--- src/resolvers/item.ts
+++ src/resolvers/item.ts
@@ -1,131 +1,131 @@
-import { getUserByUsername } from 'src/utils/db';
-import { Resolver, Query, Arg, Mutation, Ctx } from 'type-graphql';
-import { Item, ItemModel } from '../models/Item';
-import { UserModel } from '../models/User';
-import { MyContext } from '../utils/misc';
+import { getUserByUsername } from "../utils/db";
+import { Resolver, Query, Arg, Mutation, Ctx } from "type-graphql";
+import { Item, ItemModel } from "../models/Item";
+import { UserModel } from "../models/User";
+import { MyContext } from "../utils/misc";
 
 @Resolver()
 export class ItemResolver {
-    @Query(() => [ Item ], { nullable: true })
-    items() {
-        return ItemModel.find({});
+  @Query(() => [Item], { nullable: true })
+  items() {
+    return ItemModel.find({});
+  }
+
+  @Query(() => [Item])
+  getItemsByName(
+    @Arg("name", () => String)
+    itemName: string
+  ) {
+    return ItemModel.find({ name: itemName });
+  }
+
+  @Query(() => [Item])
+  getItemsByCategoryName(
+    @Arg("category", () => String)
+    itemCategoryName: string
+  ) {
+    return ItemModel.find({ category: itemCategoryName });
+  }
+  @Query(() => [Item])
+  getItemsByDepartment(
+    @Arg("department", () => String)
+    deptName: string
+  ) {
+    return ItemModel.find({ department: deptName });
+  }
+
+  @Query(() => [Item])
+  getItemById(
+    @Arg("id", () => String)
+    itemID: string
+  ) {
+    return ItemModel.findById(itemID);
+  }
+
+  @Mutation(() => String)
+  async createItem(
+    @Arg("name", () => String)
+    itemName: string,
+    @Arg("category", () => String)
+    itemCategory: string,
+    @Arg("location", () => String)
+    itemLocation: string,
+    @Arg("description", () => String)
+    itemDescription: string,
+    @Arg("image", () => String)
+    itemImage: string,
+    @Ctx() { userInfo }: MyContext
+  ) {
+    if (!userInfo || !userInfo.isAdmin) return "not authorized";
+    const user = await getUserByUsername(userInfo.username);
+    if (!user) return "not authorized";
+
+    if (itemCategory === "") {
+      itemCategory = "unassigned";
     }
 
-    @Query(() => [ Item ])
-    getItemsByName(
-        @Arg('name', () => String)
-        itemName: string
-    ) {
-        return ItemModel.find({ name: itemName });
+    const item = new ItemModel({
+      name: itemName,
+      category: itemCategory,
+      department: user.department,
+      location: itemLocation,
+      description: itemDescription,
+      image: itemImage,
+      history: {
+        name: user.username,
+        isDepartment: true,
+        timeOfTransfer: new Date(),
+      },
+    });
+
+    try {
+      await item.save();
+      return "successfully added item";
+    } catch (err) {
+      console.log(err);
+      return "unknown error, please try again";
     }
+  }
 
-    @Query(() => [ Item ])
-    getItemsByCategoryName(
-        @Arg('category', () => String)
-        itemCategoryName: string
-    ) {
-        return ItemModel.find({ category: itemCategoryName });
-    }
-    @Query(() => [ Item ])
-    getItemsByDepartment(
-        @Arg('department', () => String)
-        deptName: string
-    ) {
-        return ItemModel.find({ department: deptName });
-    }
+  @Mutation(() => String)
+  async transferItem(
+    @Arg("id", () => String)
+    itemId: string,
+    @Arg("username", () => String)
+    username: string,
+    @Ctx() { userInfo }: MyContext
+  ) {
+    if (!userInfo || !userInfo.isAdmin) return "not authorized";
 
-    @Query(() => [ Item ])
-    getItemById(
-        @Arg('id', () => String)
-        itemID: string
-    ) {
-        return ItemModel.findById(itemID);
-    }
+    const item = await ItemModel.findById(itemId);
+    if (!item) return "item doesn't exist";
 
-    @Mutation(() => String)
-    async createItem(
-        @Arg('name', () => String)
-        itemName: string,
-        @Arg('category', () => String)
-        itemCategory: string,
-        @Arg('location', () => String)
-        itemLocation: string,
-        @Arg('description', () => String)
-        itemDescription: string,
-        @Arg('image', () => String)
-        itemImage: string,
-        @Ctx() { userInfo }: MyContext
-    ) {
-        if (!userInfo || !userInfo.isAdmin) return 'not authorized';
-        const user = await getUserByUsername(userInfo.username);
-        if (!user) return 'not authorized';
+    const lastUserUsername = item.history[item.history.length - 1].name;
+    await UserModel.findOneAndUpdate(
+      { username: lastUserUsername },
+      { $pull: { items: itemId } }
+    );
 
-        if (itemCategory === '') {
-            itemCategory = 'unassigned';
-        }
+    const user = await UserModel.findOneAndUpdate(
+      { username: username },
+      { $push: { items: itemId } },
+      { new: true }
+    );
+    if (!user) return "user doesn't exist";
 
-        const item = new ItemModel({
-            name: itemName,
-            category: itemCategory,
-            department: user.department,
-            location: itemLocation,
-            description: itemDescription,
-            image: itemImage,
-            history: {
-                name: user.username,
-                isDepartment: true,
-                timeOfTransfer: new Date()
-            }
-        });
+    await ItemModel.findOneAndUpdate(
+      { _id: itemId },
+      {
+        $push: {
+          history: {
+            name: user.username,
+            isDepartment: user.isAdmin,
+            timeOfTransfer: new Date(),
+          },
+        },
+      }
+    );
 
-        try {
-            await item.save();
-            return 'successfully added item';
-        } catch (err) {
-            console.log(err);
-            return 'unknown error, please try again';
-        }
-    }
-
-    @Mutation(() => String)
-    async transferItem(
-        @Arg('id', () => String)
-        itemId: string,
-        @Arg('username', () => String)
-        username: string,
-        @Ctx() { userInfo }: MyContext
-    ) {
-        if (!userInfo || !userInfo.isAdmin) return 'not authorized';
-
-        const item = await ItemModel.findById(itemId);
-        if (!item) return "item doesn't exist";
-
-        const lastUserUsername = item.history[item.history.length - 1].name;
-        await UserModel.findOneAndUpdate(
-            { username: lastUserUsername },
-            { $pull: { items: itemId } }
-        );
-
-        const user = await UserModel.findOneAndUpdate(
-            { username: username },
-            { $push: { items: itemId } },
-            { new: true }
-        );
-        if (!user) return "user doesn't exist";
-
-        await ItemModel.findOneAndUpdate(
-            { _id: itemId },
-            {
-                $push: {
-                    history: {
-                        name: user.username,
-                        isDepartment: user.isAdmin,
-                        timeOfTransfer: new Date()
-                    }
-                }
-            }
-        );
-
-        return 'successfully added item';
-    }
+    return "successfully added item";
+  }
 }
