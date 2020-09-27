28406ff1bda2d4c6643c08ed7b2e1d00ef51d082
Archit Bhonsle <abhonsle2000@gmail.com>
2020-09-27 11:47:50 +0530

    logout and using history for redirecting

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -8,197 +8,207 @@ import { COOKIE_TAG } from '../constants';
 import { getUserByUsername } from '../utils/db';
 import { Item, ItemModel } from '../models/Item';
 
-@Resolver(User)
+@Resolver()
 export class UserResolver {
-    @Query(() => [ User ])
-    async users(@Ctx() { userInfo }: MyContext) {
-        if (userInfo && userInfo.isAdmin) return UserModel.find({});
-        return [];
+  @Query(() => [ User ])
+  async users(@Ctx() { userInfo }: MyContext) {
+    if (userInfo && userInfo.isAdmin) return UserModel.find({});
+    return [];
+  }
+
+  @Query(() => User, { nullable: true })
+  async getUser(
+    @Arg('usernameOrEmail', () => String)
+    usernameOrEmail: string,
+    @Ctx() { userInfo }: MyContext
+  ) {
+    if (!userInfo || !userInfo.isAdmin) return null;
+
+    let foundUser = null;
+    if (isEmail(usernameOrEmail)) {
+      foundUser = await UserModel.findOne({
+        email: usernameOrEmail
+      });
+    } else if (isUsername(usernameOrEmail)) {
+      foundUser = await UserModel.findOne({
+        username: usernameOrEmail
+      });
+    }
+    return foundUser;
+  }
+
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { userInfo }: MyContext) {
+    if (!userInfo) {
+      return null;
     }
 
-    @Query(() => User, { nullable: true })
-    async getUser(
-        @Arg('usernameOrEmail', () => String)
-        usernameOrEmail: string,
-        @Ctx() { userInfo }: MyContext
-    ) {
-        if (!userInfo || !userInfo.isAdmin) return null;
+    const user = await getUserByUsername(userInfo.username);
+    if (!user) return null;
 
-        let foundUser = null;
-        if (isEmail(usernameOrEmail)) {
-            foundUser = await UserModel.findOne({
-                email: usernameOrEmail
-            });
-        } else if (isUsername(usernameOrEmail)) {
-            foundUser = await UserModel.findOne({
-                username: usernameOrEmail
-            });
-        }
-        return foundUser;
+    return user;
+  }
+
+  @Query(() => [ Item ])
+  async myItems(@Ctx() { userInfo }: MyContext) {
+    if (!userInfo) {
+      return [];
+    }
+    const user = await getUserByUsername(userInfo.username);
+    if (!user) return [];
+
+    const itemIds = user.items;
+    const items = itemIds.map((itemId) => ItemModel.findById(itemId));
+
+    return items;
+  }
+
+  @Query(() => [ Item ])
+  async getUsersItems(
+    @Arg('usernameOrEmail', () => String)
+    usernameOrEmail: string,
+    @Ctx() { userInfo }: MyContext
+  ) {
+    if (!userInfo || !userInfo.isAdmin) {
+      return [];
     }
 
-    @Query(() => User, { nullable: true })
-    async me(@Ctx() { userInfo }: MyContext) {
-        if (!userInfo) {
-            return null;
-        }
-
-        const user = await getUserByUsername(userInfo.username);
-        if (!user) return null;
-
-        return user;
+    let foundUser = null;
+    if (isEmail(usernameOrEmail)) {
+      foundUser = await UserModel.findOne({
+        email: usernameOrEmail
+      });
+    } else if (isUsername(usernameOrEmail)) {
+      foundUser = await UserModel.findOne({
+        username: usernameOrEmail
+      });
     }
+    if (!foundUser) return [];
 
-    @Query(() => [ Item ])
-    async myItems(@Ctx() { userInfo }: MyContext) {
-        if (!userInfo) {
-            return [];
-        }
-        const user = await getUserByUsername(userInfo.username);
-        if (!user) return [];
+    const itemIds = foundUser.items;
+    const items = itemIds.map((itemId) => ItemModel.findById(itemId));
 
-        const itemIds = user.items;
-        const items = itemIds.map((itemId) => ItemModel.findById(itemId));
+    return items;
+  }
 
-        return items;
+  @Mutation(() => String)
+  async login(
+    @Arg('usernameOrEmail', () => String)
+    usernameOrEmail: string,
+    @Arg('password', () => String)
+    password: string,
+    @Ctx() { res }: MyContext
+  ) {
+    if (isEmail(usernameOrEmail)) {
+      const user = await UserModel.findOne({
+        email: usernameOrEmail
+      });
+      if (!user) return 'user not found';
+
+      if (await argon2.verify(user.password, password)) {
+        res.cookie(
+          COOKIE_TAG,
+          createToken({
+            username: user.username,
+            isAdmin: user.isAdmin
+          }),
+          {
+            signed: true,
+            httpOnly: true,
+            secure: false,
+            sameSite: 'lax'
+          }
+        );
+        return 'successfully logged in';
+      } else {
+        return 'wrong passoword';
+      }
+    } else if (isUsername(usernameOrEmail)) {
+      const user = await UserModel.findOne({
+        username: usernameOrEmail
+      });
+      if (!user) {
+        return 'user not found';
+      }
+      if (await argon2.verify(user.password, password)) {
+        res.cookie(
+          COOKIE_TAG,
+          createToken({
+            username: user.username,
+            isAdmin: user.isAdmin
+          }),
+          {
+            signed: true,
+            httpOnly: true,
+            secure: false,
+            sameSite: 'lax'
+          }
+        );
+        return 'successfully logged in';
+      } else {
+        return 'wrong passoword';
+      }
+    } else {
+      return 'neither a username or an email';
     }
+  }
 
-    @Query(() => [ Item ])
-    async getUsersItems(
-        @Arg('usernameOrEmail', () => String)
-        usernameOrEmail: string,
-        @Ctx() { userInfo }: MyContext
-    ) {
-        if (!userInfo || !userInfo.isAdmin) {
-            return [];
-        }
-
-        let foundUser = null;
-        if (isEmail(usernameOrEmail)) {
-            foundUser = await UserModel.findOne({
-                email: usernameOrEmail
-            });
-        } else if (isUsername(usernameOrEmail)) {
-            foundUser = await UserModel.findOne({
-                username: usernameOrEmail
-            });
-        }
-        if (!foundUser) return [];
-
-        const itemIds = foundUser.items;
-        const items = itemIds.map((itemId) => ItemModel.findById(itemId));
-
-        return items;
+  @Mutation(() => String)
+  async logout(@Ctx() { res }: MyContext) {
+    try {
+      res.clearCookie(COOKIE_TAG);
+      return 'logged out';
+    } catch (err) {
+      return 'unknown error please try again';
     }
+  }
 
-    @Query(() => String)
-    async login(
-        @Arg('usernameOrEmail', () => String)
-        usernameOrEmail: string,
-        @Arg('password', () => String)
-        password: string,
-        @Ctx() { res }: MyContext
-    ) {
-        if (isEmail(usernameOrEmail)) {
-            const user = await UserModel.findOne({
-                email: usernameOrEmail
-            });
-            if (!user) return 'user not found';
+  @Mutation(() => String)
+  async register(
+    @Arg('username', () => String)
+    username: string,
+    @Arg('email', () => String)
+    email: string,
+    @Arg('password', () => String)
+    password: string,
+    @Arg('department', () => String)
+    department: string,
+    @Arg('designation', () => String)
+    designation: string,
+    @Arg('bio', () => String)
+    bio: string,
+    @Arg('image', () => String)
+    image: string,
+    @Arg('firstname', () => String)
+    firstname: string,
+    @Arg('lastname', () => String)
+    lastname: string,
+    @Arg('isAdmin', () => Boolean)
+    isAdmin: boolean
+  ) {
+    const hashedPassword = await argon2.hash(password);
+    const newUser = new UserModel({
+      username,
+      email,
+      department,
+      designation,
+      bio,
+      image,
+      firstname,
+      lastname,
+      isAdmin,
+      password: hashedPassword
+    });
 
-            if (await argon2.verify(user.password, password)) {
-                res.cookie(
-                    COOKIE_TAG,
-                    createToken({
-                        username: user.username,
-                        isAdmin: user.isAdmin
-                    }),
-                    {
-                        signed: true,
-                        httpOnly: true,
-                        secure: false,
-                        sameSite: 'lax'
-                    }
-                );
-                return 'successfully logged in';
-            } else {
-                return 'wrong passoword';
-            }
-        } else if (isUsername(usernameOrEmail)) {
-            const user = await UserModel.findOne({
-                username: usernameOrEmail
-            });
-            if (!user) {
-                return 'user not found';
-            }
-            if (await argon2.verify(user.password, password)) {
-                res.cookie(
-                    COOKIE_TAG,
-                    createToken({
-                        username: user.username,
-                        isAdmin: user.isAdmin
-                    }),
-                    {
-                        signed: true,
-                        httpOnly: true,
-                        secure: false,
-                        sameSite: 'lax'
-                    }
-                );
-                return 'successfully logged in';
-            } else {
-                return 'wrong passoword';
-            }
-        } else {
-            return 'neither a username or an email';
-        }
+    try {
+      await newUser.save();
+      return '';
+    } catch (err) {
+      console.log(err);
+      if (err === 11000) {
+        return 'User with same username or password already exists';
+      }
+      return 'Unknown error. Try again or contact admin.';
     }
-
-    @Mutation(() => String)
-    async register(
-        @Arg('username', () => String)
-        username: string,
-        @Arg('email', () => String)
-        email: string,
-        @Arg('password', () => String)
-        password: string,
-        @Arg('department', () => String)
-        department: string,
-        @Arg('designation', () => String)
-        designation: string,
-        @Arg('bio', () => String)
-        bio: string,
-        @Arg('image', () => String)
-        image: string,
-        @Arg('firstname', () => String)
-        firstname: string,
-        @Arg('lastname', () => String)
-        lastname: string,
-        @Arg('isAdmin', () => Boolean)
-        isAdmin: boolean
-    ) {
-        const hashedPassword = await argon2.hash(password);
-        const newUser = new UserModel({
-            username,
-            email,
-            department,
-            designation,
-            bio,
-            image,
-            firstname,
-            lastname,
-            isAdmin,
-            password: hashedPassword
-        });
-
-        try {
-            await newUser.save();
-            return '';
-        } catch (err) {
-            console.log(err);
-            if (err === 11000) {
-                return 'User with same username or password already exists';
-            }
-            return 'Unknown error. Try again or contact admin.';
-        }
-    }
+  }
 }
