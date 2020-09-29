bd924aea24940343ffb99340a07966eabfce9b03
Dion Pinto <dionpinto000@gmail.com>
2020-09-29 19:46:38 +0530

    transfer func

--- client/src/pages/totalview/totalview.jsx
+++ client/src/pages/totalview/totalview.jsx
@@ -1,10 +1,17 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 //component
 import HistoryItem from "../../components/HistoryItem/HistoryItem";
 import AdminItem from "../../components/AdminItem/AdminItem";
 
-import { gql, useQuery } from "@apollo/client";
+//material ui
+import TextField from "@material-ui/core/TextField";
+
+import { gql, useQuery, useMutation } from "@apollo/client";
+import { Button, StylesProvider } from "@material-ui/core";
+
+//styles
+import styles from "./totalview.module.css";
 
 const HISTORYITEM_QUERY = gql`
   query itemById($id: String!) {
@@ -38,6 +45,12 @@ const ITEM_QUERY = gql`
   }
 `;
 
+const TRANSFER_QUERY = gql`
+  mutation transferItem($itemId: String!, $username: String!) {
+    transferItem(id: $itemId, username: $username)
+  }
+`;
+
 const Totalview = (props) => {
   let id = null;
 
@@ -61,8 +74,57 @@ const Totalview = (props) => {
     itemtot = <AdminItem data={item.getItemById} />;
   }
 
+  const [form, setForm] = useState({
+    itemId: "",
+    username: "",
+  });
+
+  const [runTransferQuery, { data }] = useMutation(TRANSFER_QUERY);
+
+  const onSubmit = (e) => {
+    runTransferQuery({
+      variables: form,
+    });
+
+    // console.log(form);
+    window.location = `/totalview/${id}`;
+  };
+
   return (
     <Fragment>
+      <div className={styles.transfer}>
+        <TextField
+          id="itemid"
+          name="itemid"
+          type="text"
+          label="item id"
+          variant="outlined"
+          value={form.itemId}
+          onChange={(e) =>
+            setForm({
+              ...form,
+              itemId: e.target.value,
+            })
+          }
+        />
+        <TextField
+          id="transferuser"
+          name="transferuser"
+          type="text"
+          label="Transer to user"
+          variant="outlined"
+          value={form.username}
+          onChange={(e) =>
+            setForm({
+              ...form,
+              username: e.target.value,
+            })
+          }
+        />
+        <Button variant="contained" color="secondary" onClick={onSubmit}>
+          Transfer
+        </Button>
+      </div>
       <h1>Item</h1>
       {itemtot}
       <h1>Item History</h1>
