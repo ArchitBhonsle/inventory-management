274b05697cca0ae6a2feb1a94d2fe8768d326ece
Archit Bhonsle <abhonsle2000@gmail.com>
2020-09-30 10:43:15 +0530

    moving to mongodb atlas

--- src/index.ts
+++ src/index.ts
@@ -9,44 +9,44 @@ import cookieParser from 'cookie-parser';
 import { getUserInfoFromToken } from './utils/jwt';
 import { MyContext } from './utils/misc';
 import { COOKIES_SECRET, COOKIE_TAG } from './constants';
-// import { seed } from './utils/seed';
+import dotenv from 'dotenv';
 
 const main = async () => {
-    await mongoose.connect('mongodb://localhost/inventory-management', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    });
+  dotenv.config();
 
-    // await seed();
+  await mongoose.connect(String(process.env.MONGO), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
 
-    const app = express();
+  const app = express();
 
-    app.use(cookieParser(COOKIES_SECRET));
+  app.use(cookieParser(COOKIES_SECRET));
 
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [ UserResolver, ItemResolver ]
-        }),
-        context: async ({ req, res }): Promise<MyContext> => {
-            const token = req.signedCookies[COOKIE_TAG];
-            const userInfo = getUserInfoFromToken(token);
-            return { userInfo, req, res };
-        }
-    });
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [ UserResolver, ItemResolver ]
+    }),
+    context: async ({ req, res }): Promise<MyContext> => {
+      const token = req.signedCookies[COOKIE_TAG];
+      const userInfo = getUserInfoFromToken(token);
+      return { userInfo, req, res };
+    }
+  });
 
-    apolloServer.applyMiddleware({
-        app,
-        cors: {
-            credentials: true,
-            origin: 'http://localhost:3000'
-        }
-    });
+  apolloServer.applyMiddleware({
+    app,
+    cors: {
+      credentials: true,
+      origin: 'http://localhost:3000'
+    }
+  });
 
-    app.listen(4000, () => {
-        console.log('🚀 at http://localhost:4000/graphql');
-    });
+  app.listen(4000, () => {
+    console.log('🚀 at http://localhost:4000/graphql');
+  });
 };
 
 main();
--- src/seed.ts
+++
@@ -1,7 +0,0 @@
-export const seedUsers = [
-    {
-        username: 'abc',
-        password: 'abc',
-        email: 'abc@abc.com'
-    }
-];
