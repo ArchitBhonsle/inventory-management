fd7dae8b29a3b938cdef9639f7620fe8f19d8586
Archit Bhonsle <abhonsle2000@gmail.com>
2020-09-30 12:39:12 +0530

    adding dist

---
+++ dist/constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.COOKIE_TAG = exports.COOKIES_SECRET = exports.ACCESS_TOKEN_KEY = exports.__prod__ = void 0;
+exports.__prod__ = process.env.NODE_ENV === 'production';
+exports.ACCESS_TOKEN_KEY = 'skillenza-assam-hackathon';
+exports.COOKIES_SECRET = 'skillenza-assam-hackathon';
+exports.COOKIE_TAG = 'skillenza-assam-hackathon';
+//# sourceMappingURL=constants.js.map
