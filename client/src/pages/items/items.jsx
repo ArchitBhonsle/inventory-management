070f4b28dc48a8d9b24a96c49a6c85b495425330
Dion Pinto <dionpinto000@gmail.com>
2020-09-30 08:32:36 +0530

    added additem to items page

--- client/src/pages/items/items.jsx
+++ client/src/pages/items/items.jsx
@@ -1,20 +1,21 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from "react";
 
 //material ui
-import { Grid, Typography } from '@material-ui/core';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import { Grid, Typography } from "@material-ui/core";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 //components
-import AdminItem from '../../components/AdminItem/AdminItem';
+import AdminItem from "../../components/AdminItem/AdminItem";
+import AdminAddItem from "../../components/AdminAddItem/AdminAddItem";
 
 //graphql
-import { gql, useQuery, useLazyQuery } from '@apollo/client';
+import { gql, useQuery, useLazyQuery } from "@apollo/client";
 
 //styles
-import styles from './items.module.css';
+import styles from "./items.module.css";
 
 const ME_QUERY = gql`
   query {
@@ -25,6 +26,14 @@ const ME_QUERY = gql`
       isAdmin
       department
       image
+    }
+  }
+`;
+
+const CAT_QUERY = gql`
+  query getItemsByDepartment($department: String!) {
+    getItemsByDepartment(department: $department) {
+      category
     }
   }
 `;
@@ -42,6 +51,7 @@ const ITEMS_QUERY = gql`
       name
       location
       department
+      image
     }
   }
 `;
@@ -51,36 +61,63 @@ const Items = () => {
   let department = undefined;
   if (meData && meData.me) department = meData.me.department;
 
-  const [ category, setCategory ] = useState('');
-  const [ runItemsQuery, { data } ] = useLazyQuery(ITEMS_QUERY);
-  const [ items, setItems ] = useState(null);
+  const [category, setCategory] = useState("");
+  const [runItemsQuery, { data }] = useLazyQuery(ITEMS_QUERY);
+  const [items, setItems] = useState(null);
 
-  useEffect(
-    () => {
-      if (department) {
-        runItemsQuery({
-          variables : { department, category }
-        });
-      }
-    },
-    [ category, department, runItemsQuery ]
-  );
+  useEffect(() => {
+    if (department) {
+      runItemsQuery({
+        variables: { department, category },
+      });
+    }
+  }, [category, department, runItemsQuery]);
 
-  useEffect(
-    () => {
-      if (data && data.getItemsByDepartmentAndCategory) {
-        setItems(
-          data.getItemsByDepartmentAndCategory.map((item, ind) => {
-            return <AdminItem data={item} key={ind} />;
-          })
-        );
-      }
-    },
-    [ data ]
-  );
+  const [runCatQuery, { data: catlist }] = useLazyQuery(CAT_QUERY);
+
+  useEffect(() => {
+    if (department) {
+      runCatQuery({
+        variables: { department },
+      });
+    }
+  }, [department, runCatQuery]);
+
+  useEffect(() => {
+    if (data && data.getItemsByDepartmentAndCategory) {
+      setItems(
+        data.getItemsByDepartmentAndCategory.map((item, ind) => {
+          return <AdminItem data={item} key={ind} />;
+        })
+      );
+    }
+  }, [data]);
+
+  var categoryItemSet = new Set();
+
+  let categoryItems = null;
+
+  useEffect(() => {
+    if (catlist && catlist.getItemsByDepartment) {
+      catlist.getItemsByDepartment.map((item, ind) => {
+        categoryItemSet.add(item.category);
+      });
+    }
+
+    const categoryItemsArray = Array.from(categoryItemSet);
+
+    categoryItems = categoryItemsArray.map((item) => {
+      return <MenuItem value={item}>{item}</MenuItem>;
+    });
+  }, [catlist]);
 
   return (
     <Fragment>
+      <div className={styles.additems}>
+        <h1>Add Items</h1>
+        <AdminAddItem />
+      </div>
+
       <Typography variant="h3" color="textSecondary" className={styles.header}>
         <FormControl className={styles.formControl}>
           <InputLabel>Category</InputLabel>
@@ -95,6 +132,7 @@ const Items = () => {
             <MenuItem value="item1">u</MenuItem>
             <MenuItem value="v">v</MenuItem>
             <MenuItem value="w">w</MenuItem>
+            {categoryItems}
           </Select>
         </FormControl>
       </Typography>
